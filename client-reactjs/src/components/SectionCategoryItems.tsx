9069578d553fe5655a47e50410fc3f6369321d12
R4AYA4R <shilovich.yaroslav@gmail.com>
2025-02-17 12:45:16 +0200

    added sectionCategoryItems

---
+++ client-reactjs/src/components/SectionCategoryItems.tsx
@@ -0,0 +1,31 @@
+
+const SectionCategoryItems = () => {
+
+    return(
+        <section className="sectionCategoryItems">
+            <div className="container">
+                <div className="sectionCategoryItems__inner">
+                    <div className="sectionCategoryItems__item">
+                        <img src="/images/sectionCategoryItems/list02.png" alt="" className="sectionCategoryItems__item-img" />
+                        <p className="sectionCategoryItems__item-text">Fruits & Vegetables</p>
+                    </div>
+                    <div className="sectionCategoryItems__item">
+                        <img src="/images/sectionCategoryItems/list08.png" alt="" className="sectionCategoryItems__item-img" />
+                        <p className="sectionCategoryItems__item-text">Beverages</p>
+                    </div>
+                    <div className="sectionCategoryItems__item">
+                        <img src="/images/sectionCategoryItems/list11.png" alt="" className="sectionCategoryItems__item-img" />
+                        <p className="sectionCategoryItems__item-text">Meats & Seafood</p>
+                    </div>
+                    <div className="sectionCategoryItems__item">
+                        <img src="/images/sectionCategoryItems/list17.png" alt="" className="sectionCategoryItems__item-img" />
+                        <p className="sectionCategoryItems__item-text">Breads & Bakery</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+
+}
+
+export default SectionCategoryItems;
