8f0587677986fe2f9579f8800a8b744a71dda62c
R4AYA4R <shilovich.yaroslav@gmail.com>
2025-03-14 14:04:44 +0200

    added filter price to catalog page and to node js

--- client-reactjs/src/pages/Catalog.tsx
+++ client-reactjs/src/pages/Catalog.tsx
@@ -4,22 +4,22 @@ import ReactSlider from 'react-slider'; // импортируем ReactSlider и
 import ProductItemCatalog from "../components/ProductItemCatalog";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { IProduct } from "../types/types";
+import { IProduct, IResponseCatalog } from "../types/types";
 
 const Catalog = () => {
 
-    const [searchValue,setSearchValue] = useState(''); // состояние для инпута поиска
+    const [searchValue, setSearchValue] = useState(''); // состояние для инпута поиска
 
     const [filterCategories, setFilterCategories] = useState('');
 
-    const [filterPrice, setFilterPrice] = useState<number[]>([0, 50]); // массив для значений нашего инпута range(ReactSlider),первым значением указываем значение для первого ползунка у этого инпута,а вторым для второго,указываем ему в generic тип как number и что это массив [],иначе выдает ошибку
+    const [priceFilterMax, setPriceFilterMax] = useState(0); // состояние для максимальной цены товара,которое посчитали на бэкэнде и поместили в состояние priceFilterMax,указываем дефолтное значение 0,иначе не работает,так как выдает ошибки,что для ReactSlider нельзя назначить значение с типом undefined и тд
 
     const [activeSortBlock, setActiveSortBlock] = useState(false);
 
     const [sortBlockValue, setSortBlockValue] = useState('');
 
     // делаем запрос на сервер с помощью react query при запуске страницы и описываем здесь функцию запроса на сервер,берем isFetching у useQuery,чтобы отслеживать,загружается ли сейчас запрос на сервер,разница isFetching и isLoading в том,что isFetching всегда будет проверять загрузку запроса на сервер при каждом запросе,а isLoading будет проверять только первый запрос на сервер,в данном случае нужен isFetching,так как идут повторные запросы на сервер
-    const { data,refetch } = useQuery({
+    const { data, refetch, isFetching } = useQuery({
         queryKey: ['getAllProducts'],
         queryFn: async () => {
 
@@ -33,26 +33,42 @@ const Catalog = () => {
 
             // }
 
-            // указываем тип данных,который придет от сервера как тип на основе нашего интерфейса IResponseCatalog,у этого объекта будут поля products(объекты товаров из базы данных для отдельной страници пагинации) и allProducts(все объекты товаров из базы данных без лимитов и состояния текущей страницы,то есть без пагинации,чтобы взять потом количество этих всех объектов товаров и использовать для пагинации),вместо url будет подставлено значение,которое есть у нашей переменной url
-            const response = await axios.get<IProduct[]>(url,{
-                params:{
+            // если минимальное значение фильтра цены(filterPrice[0]) цены true(то есть это значение есть и в нем есть какое-то значение,делаем эту проверку иначе выдает ошибку,что filterPrice[0] может быть undefined) и (filterPrice[0]) больше 0,то добавляем query параметр minPrice к url со значением этого минимального значения фильтра цены(filterPrice[0])
+            if (filterPrice[0] && filterPrice[0] > 0) {
 
-                    category:filterCategories // указываем query параметр category со значением filterCategories(даже если filterCategories пустая строка,то оно также правильно обрабатывается и проверки дополнительные здесь не нужны,мы проверяем на бэкэнде,не равен ли этот query параметр category пустой строке)
+                url += `&minPrice=${filterPrice[0]}`;
+
+            }
+
+            // если data?.maxPriceAllProducts(максимальное значение цены товара,которое мы посчитали на бэкэнде и поместили в поле maxPriceAllProducts) true,то есть поле maxPriceAllProducts есть и в нем есть какое-то значение(делаем эту проверку,так как выдает ошибку,что data?.maxPriceAllProducts может быть undefined,но мы и так будем изменять фильтр цены когда объекты товаров загрузятся,и это уже будет повторный запрос на сервер,поэтому значение data?.maxPriceAllProducts уже будет и поэтому это подходит) и filterPrice[1] (максимальное значение фильтра цены) true(то есть это значение есть и в нем есть какое-то значение,делаем эту проверку иначе выдает ошибку,что filterPrice[1] может быть undefined) и и если filterPrice[1] (максимальное значение фильтра цены) меньше data?.maxPriceAllProducts(максимальное значение цены товара,которое мы посчитали на бэкэнде и поместили в поле maxPriceAllProducts),то добавляем query параметр maxPrice к url со значением этого максимального значения фильтра цены(filterPrice[1])
+            if (data?.maxPriceAllProducts && filterPrice[1] && filterPrice[1] < data?.maxPriceAllProducts) {
+
+                url += `&maxPrice=${filterPrice[1]}`;
+
+            }
+
+            // указываем тип данных,который придет от сервера как тип на основе нашего интерфейса IResponseCatalog,у этого объекта будут поля products(объекты товаров из базы данных для отдельной страници пагинации) и allProducts(все объекты товаров из базы данных без лимитов и состояния текущей страницы,то есть без пагинации,чтобы взять потом количество этих всех объектов товаров и использовать для пагинации),вместо url будет подставлено значение,которое есть у нашей переменной url
+            const response = await axios.get<IResponseCatalog>(url, {
+                params: {
+
+                    category: filterCategories // указываем query параметр category со значением filterCategories(даже если filterCategories пустая строка,то оно также правильно обрабатывается и проверки дополнительные здесь не нужны,мы проверяем на бэкэнде,не равен ли этот query параметр category пустой строке)
 
                 }
             });  // делаем запрос на сервер для получения всех товаров,указываем в типе в generic наш тип на основе интерфейса IProduct,указываем,что это массив(то есть указываем тип данных,которые придут от сервера)
 
             console.log(response.data);
 
-            return response; // возвращаем этот массив объектов товаров(он будет помещен в поле data у data,которую мы берем из этого useQuery)
+            return response.data; // возвращаем response.data,то есть объект data,который получили от сервера,в котором есть поля products и allProducts
 
         }
 
     })
 
+    const [filterPrice, setFilterPrice] = useState([0, priceFilterMax]); // массив для значений нашего инпута range(ReactSlider),первым значением указываем значение для первого ползунка у этого инпута,а вторым для второго, ставим изначальное значение для второго ползунка инпута как priceFilterMax(максимальная цена товарв из всех,которую посчитали на бэкэнде),чтобы сразу показывалось,что это максимальное значение цены,не указываем здесь конкретно data?.maxPriceAllProducts,так как тогда выдает ошибки,что для ReactSlider нельзя назначить значение с типом undefined и тд
+
 
     // функция при изменении значения инпута поиска,указываем параметр e как тип ChangeEvent у в generic у него указываем HTMLInputElement
-    const searchValueHandler = (e:ChangeEvent<HTMLInputElement>) => {
+    const searchValueHandler = (e: ChangeEvent<HTMLInputElement>) => {
 
         setSearchValue(e.target.value);
 
@@ -67,20 +83,55 @@ const Catalog = () => {
 
     }
 
+    // при изменении data?.products изменяем значение priceFilterMax на data?.maxPriceAllProducts(максимальное значение цены товара,которое посчитали на бэкэнде)
+    useEffect(() => {
+
+        // если data?.maxPriceAllProducts true,то есть data?.maxPriceAllProducts есть и в нем есть какое-то значение,делаем эту проверку,иначе выдает ошибку,что data?.maxPriceAllProducts может быть undefined
+        if (data?.maxPriceAllProducts) {
+
+            setPriceFilterMax(data?.maxPriceAllProducts);
+
+        }
+
+        console.log(data?.maxPriceAllProducts)
+
+    }, [data?.products]);
+
+    // при изменении priceFilterMax изменяем значение filterPrice,возвращаем массив,первым элементом указываем предыдущее значение по индексу 0(то есть минимальное значение фильтра цены) и второй элемент указываем со значением как priceFilterMax(делаем эти манипуляции с priceFilterMax,иначе выдает ошибку,что для ReacSlider нельзя указать значение с типом undefined и тд)
+    useEffect(() => {
+
+        setFilterPrice((prev) => [prev[0], priceFilterMax]);
+
+        console.log(filterPrice)
+
+    }, [priceFilterMax])
+
     // указываем в массиве зависимостей этого useEffect data?.products(массив объектов блюд для отдельной страницы пагинации),чтобы делать повторный запрос на получения объектов товаров при изменении data?.products,в данном случае это для пагинации,если не указать data?.products,то пагинация при запуске страницы не будет работать, также делаем повторный запрос на сервер уже с измененным значение searchValue(чтобы поисковое число(число товаров,которое изменяется при поиске) показывалось правильно,когда вводят что-то в поиск)
-    useEffect(()=>{
+    useEffect(() => {
 
         refetch();  // делаем повторный запрос на получение товаров при изменении data?.products, searchValue(значение инпута поиска),filterCategories и других фильтров,а также при изменении состояния текущей страницы пагинации 
 
-    },[searchValue,filterCategories])
+    }, [searchValue, filterCategories])
 
 
     // при изменении searchValue,то есть когда пользователь что-то вводит в инпут поиска,то изменяем filterCategory на пустую строку и остальные фильтры тоже,соответственно будет сразу идти поиск по всем товарам,а не в конкретной категории или определенных фильтрах,но после поиска можно будет результат товаров по поиску уже отфильтровать по категориям и тд
-    useEffect(()=>{
+    useEffect(() => {
 
         setFilterCategories('');
 
-    },[searchValue])
+    }, [searchValue])
+
+    // при изменении состояния filterPrice,то есть когда пользователь начал изменять значение фильтра цены(то есть начал крутить ползунки для изменения фильтра цены),то делаем запрос на сервер на получение объектов товаров уже с новым фильтром цены,отслеживаем это,чтобы делать запрос на сервер только после того,когда закончил грузиться предыдущий запрос на сервер(лучше еще отслеживать когда пользователь отпустил ползунок фильтра цены и только тогда делать запрос,но в данном случае используем React Slider и там не удобно это отслеживать,но и так нормально),если это не отслеживать,то будут лететь кучи запросов на сервер при изменении значения фильтра цены
+    useEffect(() => {
+
+        // если isFetching false,то есть запрос на сервер для получения объектов блюд сейчас не грузится(делаем эту проверку,чтобы не было много запросов и долго не грузились данные после того,как пользователь зашел на страницу товара и вернулся в каталог)
+        if (!isFetching) {
+
+            refetch();
+
+        }
+
+    }, [filterPrice])
 
     return (
         <main className="main">
@@ -141,9 +192,9 @@ const Catalog = () => {
 
                                         trackClassName="inputRangeSlider__track"
 
-                                        defaultValue={[0, 50]} // поле для дефолтного значения минимального(первый элемент массива) и максимального(второй элемент массива)
+                                        defaultValue={filterPrice} // поле для дефолтного значения минимального(первый элемент массива) и максимального(второй элемент массива),указываем этому полю значение как наш массив filterPrice(массив чисел для минимального(первый элемент массива) и максимального(второй элемент массива) значения)
 
-                                        max={50} // поле для максимального значения
+                                        max={data?.maxPriceAllProducts} // поле для максимального значения
 
                                         min={0} // поле для минимального значения
 
@@ -168,14 +219,14 @@ const Catalog = () => {
                                     />
 
                                     {/* выводим минимальное текущее значение инпута range (наш ReactSlider) по индексу 0 из нашего массива filterPrice (filterPrice[0]) и выводим максимальное текущее значение по индексу 1 из нашего массива filterPrice (filterPrice[1]) */}
-                                    <p className="priceFilterBlock__text">Price: ${filterPrice[0]} - ${filterPrice[1]}</p>
+                                    <p className="priceFilterBlock__text">Price: ${filterPrice[0]} - ${filterPrice[1].toFixed(0)}</p>
 
                                 </div>
                             </div>
                             <div className="sectionCatalog__mainBlock-productsBlock">
                                 <div className="sectionCatalog__productsBlock-searchBlock">
                                     <div className="productsBlock__searchBlock-inputBlock">
-                                        <input type="text" className="productsBlock__searchBlock-input" placeholder="Search for products..." value={searchValue} onChange={searchValueHandler}/>
+                                        <input type="text" className="productsBlock__searchBlock-input" placeholder="Search for products..." value={searchValue} onChange={searchValueHandler} />
                                         <img src="/images/sectionCatalog/SearchImg.png" alt="" className="searchBlock__inputBlock-inputImg" />
                                     </div>
                                     <div className="searchBlock__sortBlock">
@@ -227,14 +278,14 @@ const Catalog = () => {
                                                 </div>
                                             }
 
-                                            {/* если элемент по индексу 0 из массива filterPrice не равно 0 или элемент по индексу 1 из массива filterPrice не равен 50(то есть это не дефолтные значения,то есть пользователь изменил фильтр цены),то показываем фильтр цены,в данном случае делаем условие именно таким образом(после условия ставим знак вопроса ? (то есть если условие выполняется), а потом ниже в коде ставим двоеточие : (то есть в противоположном случае,если это условие не выполняется) и пустую строку '' (то есть не показываем ничего) ),иначе не работает правильно условие */}
-                                            {filterPrice[0] !== 0 || filterPrice[1] !== 50 ?
+                                            {/* если элемент по индексу 0 из массива filterPrice не равно 0 или элемент по индексу 1 из массива filterPrice меньше priceFilterMax(максимальное значение цены товара,которое посчитали на бэкэнде и поместили в состояние priceFilterMax) (то есть это не дефолтные значения,то есть пользователь изменил фильтр цены),то показываем фильтр цены,в данном случае делаем условие именно таким образом(после условия ставим знак вопроса ? (то есть если условие выполняется), а потом ниже в коде ставим двоеточие : (то есть в противоположном случае,если это условие не выполняется) и пустую строку '' (то есть не показываем ничего) ),иначе не работает правильно условие */}
+                                            {filterPrice[0] !== 0 || filterPrice[1] < priceFilterMax ?
                                                 <div className="filtersBlock__leftBlock-item">
 
-                                                    <p className="filtersBlock__item-text">Price: {filterPrice[0]} - {filterPrice[1]}</p>
+                                                    <p className="filtersBlock__item-text">Price: {filterPrice[0]} - {(filterPrice[1]).toFixed(0)}</p>
 
                                                     {/* в onClick(при нажатии на кнопку) изменяем состояние filterPrice на массив со значениями 0 и 50(дефолтные значения минимального и максимального значения фильтра цены),то есть убираем фильтр цены */}
-                                                    <button className="filtersBlock__item-btn" onClick={() => setFilterPrice([0, 50])}>
+                                                    <button className="filtersBlock__item-btn" onClick={() => setFilterPrice([0, priceFilterMax])}>
                                                         <img src="/images/sectionCatalog/CrossImg.png" alt="" className="filtersBlock__item-btnIMg" />
                                                     </button>
                                                 </div> : ''
@@ -264,9 +315,10 @@ const Catalog = () => {
 
                                     <div className="sectionCatalog__productsBlock-productsItems">
 
-                                        {data?.data.map(product =>
+                                        {/* проходимся по массиву объектов товаров products,указываем data?.products,так как от сервера в поле data приходит объект с полями products(объекты товаров из базы данных для отдельной страници пагинации) и allProducts(все объекты товаров из базы данных без лимитов и состояния текущей страницы,то есть без пагинации,чтобы взять потом количество этих всех объектов блюд и использовать для пагинации) */}
+                                        {data?.products.map(product =>
 
-                                            <ProductItemCatalog key={product._id} product={product}/>
+                                            <ProductItemCatalog key={product._id} product={product} />
 
                                         )}
 
--- client-reactjs/src/types/types.ts
+++ client-reactjs/src/types/types.ts
@@ -16,3 +16,10 @@ export interface IProduct{
     descImages:string[] // указываем это поле для массива названий картинок с типом string,это будут дополнительные картинки товара для страницы о товаре(ProductItemPage)
 
 }
+
+// создаем и экспортируем тип для объекта,который придет от сервера при запросе на товары каталога,указываем поле products(массив объектов товаров) с типом на основе нашего интерфейса IProduct[],также для allProducts(в нем будут все объекты товаров без лимитов и состояния страницы пагинации) и поле maxPriceAllProducts(то есть максимальное значение цены товара,которое мы посчитали на бэкэнде и поместили в поле maxPriceAllProducts) с типом number
+export interface IResponseCatalog{
+    allProducts:IProduct[],
+    products:IProduct[],
+    maxPriceAllProducts:number
+}
