5a85803d3fe5ff8f790d15d71f5b0afbe62efc04
R4AYA4R <shilovich.yaroslav@gmail.com>
2025-04-06 12:00:12 +0300

    added some cart

---
+++ client-reactjs/src/components/SectionCartTop.tsx
@@ -0,0 +1,28 @@
+
+import { RefObject, useRef } from "react";
+import { useIsOnScreen } from "../hooks/useIsOnScreen";
+
+const SectionCartTop = () => {
+
+    const sectionBestSellers = useRef<HTMLElement>(null); // создаем ссылку на html элемент и помещаем ее в переменную sectionTopRef,указываем тип в generic этому useRef как HTMLElement(иначе выдает ошибку),указываем в useRef null,так как используем typeScript
+
+    const onScreen = useIsOnScreen(sectionBestSellers as RefObject<HTMLElement>); // вызываем наш хук useIsOnScreen(),куда передаем ссылку на html элемент(в данном случае на sectionTop),указываем тип этой ссылке на html элемент как RefObject<HTMLElement> (иначе выдает ошибку),и этот хук возвращает объект состояний,который мы помещаем в переменную onScreen
+
+
+    return (
+        <section id="sectionBestSellers" className={onScreen.sectionBestSellersIntersecting ? "sectionBestSellers sectionBestSellers__active sectionUserPageTop" : "sectionBestSellers sectionUserPageTop"} ref={sectionBestSellers}>
+            <div className="container">
+                <div className="sectionUserPageTop__inner">
+                    <div className="sectionCatalog__topBlock sectionUserPageTop__topBlock">
+                        <p className="sectionCatalog__topBlock-title">Home</p>
+                        <img src="/images/sectionCatalog/ArrowRight.png" alt="" className="sectionCatalog__topBlock-img" />
+                        <p className="sectionCatalog__topBlock-subtitle">Cart</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+
+}
+
+export default SectionCartTop;
--- client-reactjs/src/pages/Cart.tsx
+++ client-reactjs/src/pages/Cart.tsx
@@ -1,9 +1,57 @@
+import SectionCartTop from "../components/SectionCartTop";
 
 const Cart = () => {
 
     return (
         <main className="main">
-            Cart
+            <SectionCartTop/>
+            <section className="sectionCart">
+                <div className="container">
+                    <div className="sectionCart__inner">
+                        <div className="sectionCart__table">
+                            <div className="sectionCart__table-names">
+                                <p className="sectionCart__table-name">Products</p>
+                                <p className="sectionCart__table-name">Price</p>
+                                <p className="sectionCart__table-name">Quantity</p>
+                                <p className="sectionCart__table-name">Subtotal</p>
+                            </div>
+                            <div className="sectionCart__table-mainBlock">
+                                
+                                Products
+                                
+                                <div className="sectionCart__table-bottomBlock">
+                                    <button className="sectionCart__table-bottomBlockClearBtn">Clear Cart</button>
+
+                                    <button className="sectionCart__table-bottomBlockUpdateBtn">Update Cart</button>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div className="sectionCart__bill">
+                            <div className="bill__topBlock">
+                                <div className="bill__topBlock-item">
+                                    <p className="bill__topBlock-itemText">Cart Subtotal</p>
+                                    <p className="bill__topBlock-itemSubText">$15.00</p>
+                                </div>
+                                <div className="bill__topBlock-item">
+                                    <p className="bill__topBlock-itemTextGrey">Shipping Charge</p>
+                                    <p className="bill__topBlock-itemSubTextGrey">$10.00</p>
+                                </div>
+                            </div>
+                            <div className="bill__bottomBlock">
+                                <div className="bill__bottomBlock-item">
+                                    <p className="bill__bottomBlock-itemText">Total</p>
+                                    <p className="bill__bottomBlock-itemSubText">$25.00</p>
+                                </div>
+                                <button className="bill__bottomBlock-btn">
+                                    <p className="bill__bottomBlock-btnText">Proceed to Checkout</p>
+                                    <img src="/images/sectionCart/CheckSquareOffset.png" alt="" className="bill__bottomBlock-btnImg" />
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </main>
     )
 
