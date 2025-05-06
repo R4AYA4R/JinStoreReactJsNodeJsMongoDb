e5242f02497f6b96a53571b9f380f899fffe3766
R4AYA4R <shilovich.yaroslav@gmail.com>
2025-05-06 13:36:24 +0300

    added mobile menu to header

--- client-reactjs/src/components/header/Header.tsx
+++ client-reactjs/src/components/header/Header.tsx
@@ -7,11 +7,13 @@ import { useQuery } from '@tanstack/react-query';
 import { AuthResponse, IProductCart, IProductsCartResponse } from '../../types/types';
 import axios from 'axios';
 import { useTypedSelector } from '../../hooks/useTypedSelector';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useActions } from '../../hooks/useActions';
 import { API_URL } from '../../http/http';
 
 const Header = () => {
+
+    const [activeMobileMenu,setActiveMobileMenu] = useState(false);
 
     const { isAuth, user, isLoading } = useTypedSelector(state => state.userSlice); // указываем наш слайс(редьюсер) под названием userSlice и деструктуризируем у него поле состояния isAuth и тд,используя наш типизированный хук для useSelector
 
@@ -95,14 +97,14 @@ const Header = () => {
                     </NavLink>
                     <ul className={cl.header__menu}>
                         <li className={cl.header__menuItem}>
-                            {/* используем тут NavLink,чтобы при клике на эту ссылку перекинуть пользователя по маршруту /(то есть на главную страницу),указываем это в to,также используем тут NavLink,вместо обычного Link,чтобы отслеживать активное состояние этой ссылки,то есть находится ли пользователь на этой странице,для которой эта ссылка или нет,чтобы указать ей активный класс */}
-                            <NavLink to="/" className={cl.header__menuLink}>Home</NavLink>
+                            {/* используем тут NavLink,чтобы при клике на эту ссылку перекинуть пользователя по маршруту /(то есть на главную страницу),указываем это в to,также используем тут NavLink,вместо обычного Link,чтобы отслеживать активное состояние этой ссылки,то есть находится ли пользователь на этой странице,для которой эта ссылка или нет,чтобы указать ей активный класс,также отслеживаем активную сейчас страницу и с помощью isActive у NavLink указываем,если isActive true,то указываем классы для активной кнопки текущей страницы,в другом случае обычные классы */}
+                            <NavLink to="/" className={({isActive}) => isActive ? `${cl.header__menuLink} ${cl.header__menuLinkActive}` : cl.header__menuLink}>Home</NavLink>
                         </li>
                         <li className={cl.header__menuItem}>
-                            <NavLink to="/catalog" className={cl.header__menuLink}>Catalog</NavLink>
+                            <NavLink to="/catalog" className={({isActive}) => isActive ? `${cl.header__menuLink} ${cl.header__menuLinkActive}` : cl.header__menuLink}>Catalog</NavLink>
                         </li>
                         <li className={cl.header__menuItem}>
-                            <NavLink to="/aboutUs" className={cl.header__menuLink}>About Us</NavLink>
+                            <NavLink to="/aboutUs" className={({isActive}) => isActive ? `${cl.header__menuLink} ${cl.header__menuLinkActive}` : cl.header__menuLink}>About Us</NavLink>
                         </li>
                         <li className={cl.header__menuItem}>
                             <NavLink to="/userPage" className={cl.header__menuLink}>
@@ -117,6 +119,46 @@ const Header = () => {
                             </NavLink>
                         </li>
                     </ul>
+
+                    {/* если activeMobileMenu true (то есть сейчас открыто мобильное меню),то показываем блок див и onClick указываем,что изменяем состояние activeMobileMenu на false,то есть будем закрывать мобильное меню по клику на другую область,кроме этого меню,чтобы оно закрылось не только по кнопке,но и по области вокруг,в другом случае этот блок показан не будет */}
+                    {activeMobileMenu && 
+                        <div className={cl.header__menuMobileCloseDiv} onClick={()=>setActiveMobileMenu(false)}></div>
+                    }
+
+                    <ul className={activeMobileMenu ? `${cl.header__menuMobile} ${cl.header__menuMobileActive}` : cl.header__menuMobile}>
+                        <li className={cl.header__menuItem}>
+                            {/* используем тут NavLink,чтобы при клике на эту ссылку перекинуть пользователя по маршруту /(то есть на главную страницу),указываем это в to,также используем тут NavLink,вместо обычного Link,чтобы отслеживать активное состояние этой ссылки,то есть находится ли пользователь на этой странице,для которой эта ссылка или нет,чтобы указать ей активный класс,также отслеживаем активную сейчас страницу и с помощью isActive у NavLink указываем,если isActive true,то указываем классы для активной кнопки текущей страницы,в другом случае обычные классы */}
+                            <NavLink to="/" className={({isActive}) => isActive ? `${cl.header__menuLink} ${cl.header__menuLinkActive}` : cl.header__menuLink}>Home</NavLink>
+                        </li>
+                        <li className={cl.header__menuItem}>
+                            <NavLink to="/catalog" className={({isActive}) => isActive ? `${cl.header__menuLink} ${cl.header__menuLinkActive}` : cl.header__menuLink}>Catalog</NavLink>
+                        </li>
+                        <li className={cl.header__menuItem}>
+                            <NavLink to="/aboutUs" className={({isActive}) => isActive ? `${cl.header__menuLink} ${cl.header__menuLinkActive}` : cl.header__menuLink}>About Us</NavLink>
+                        </li>
+
+                        <li className={`${cl.header__menuItem} ${cl.header__menuItemUser}`}>
+                            <NavLink to="/userPage" className={cl.header__menuLink}>
+                                <img src="/images/header/Vector.png" alt="" className={cl.menuLink__img} />
+                            </NavLink>
+                        </li>
+                        <li className={`${cl.header__menuItem} ${cl.header__menuItemCart}`}>
+                            {/* указываем 2 модульных класса таким образом в виде строки с такими ковычками ``,и между ними указываем пробел */}
+                            <NavLink to="/cart" className={`${cl.header__menuLink} ${cl.header__menuLinkCart}`}>
+                                <img src="/images/header/Cart.png" alt="" className={cl.menuLink__img} />
+                                <span className={cl.menuLink__span}>{dataProductsCart?.allProductsCart.length}</span>
+                            </NavLink>
+                        </li>
+                    </ul>
+
+                    <button className={cl.header__menuBtn} onClick={()=>setActiveMobileMenu((prev) => !prev)}>
+                        {/* если activeMobileMenu true(то есть сейчас открыто мобильное меню),то показываем активные классы для каждого спана отдельно,так как делаем им такую анимацию,в другом случае указываем обычный класс */}
+                        <span className={activeMobileMenu ? `${cl.menuBtn__span} ${cl.menuBtn__spanActive1}` : cl.menuBtn__span}></span>
+                        <span className={activeMobileMenu ? `${cl.menuBtn__span} ${cl.menuBtn__spanActive2}` : cl.menuBtn__span}></span>
+                        <span className={activeMobileMenu ? `${cl.menuBtn__span} ${cl.menuBtn__spanActive3}` : cl.menuBtn__span}></span>
+                        <span className={activeMobileMenu ? `${cl.menuBtn__span} ${cl.menuBtn__spanActive1}` : cl.menuBtn__span}></span>
+                    </button>
+
                 </div>
             </div>
         </header>
